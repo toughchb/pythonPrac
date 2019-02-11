f990904261714abbd2d052cda8cc47a22a1b019b
ggang2000 <ggang2000@gmail.com>
2019-02-11 19:11:33 +0900

    수정

--- docxTest/textprac.py
+++ docxTest/textprac.py
@@ -2,16 +2,22 @@ import sys
 from googletrans import Translator
 translator = Translator()
 
-fr = open("input.txt",'r')
-fw = open("ouput.txt",'w')
+print("Please enter input text file name.")
+source = input().strip()
+
+fr = open( source + ".txt",'r', encoding='UTF8')
+fw = open("ouput.txt",'w', encoding='UTF8')
 
 while True:
     r_line = fr.readline()
-    outline = translator.translate(r_line, src='en', dest='ko')
-    print(type(r_line))
-    print(r_line)
     if not r_line: break
+    #print(r_line)
+    outline = translator.translate(r_line, src='en', dest='ko')
+    #print(outline.text)
+    outline.text = outline.text + '\n'
+    fw.write(r_line)
     fw.write(outline.text)
+
 
 fr.close()
 fw.close()
