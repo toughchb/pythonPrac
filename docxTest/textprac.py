1cdf1c7835e2e8d1b048a88db8dde4a3a0b0efa9
rose800 <gusqud87@naver.com>
2019-02-26 00:15:07 +0900

    구글번역 테스트

--- docxTest/textprac.py
+++ docxTest/textprac.py
@@ -1,22 +1,28 @@
 import sys
+import time
 from googletrans import Translator
 translator = Translator()
 
-print("Please enter input text file name.")
-source = input().strip()
+#print("Please enter input text file name.")
+#source = input().strip()
 
-fr = open( source + ".txt",'r', encoding='UTF8')
-fw = open("ouput.txt",'w', encoding='UTF8')
+#fr = open("input.txt", 'r')
+fr = open("input.txt", 'r', encoding='UTF8')
 
-while True:
+#fw = open("ouput.txt", 'w')
+fw = open("ouput.txt", 'w', encoding='UTF8')
+count =0;
+while (count < 1000):
+    time.sleep(0.05)
     r_line = fr.readline()
     if not r_line: break
-    #print(r_line)
+    print(r_line)
     outline = translator.translate(r_line, src='en', dest='ko')
-    #print(outline.text)
+    print(outline.text)
     outline.text = outline.text + '\n'
     fw.write(r_line)
     fw.write(outline.text)
+    count += 1
 
 
 fr.close()
